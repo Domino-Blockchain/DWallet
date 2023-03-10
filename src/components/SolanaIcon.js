b4272e15c64f5ac04a46327e08e11aa5dc0457b8
rs-Nenad <jovanovski.n@redsteep.com>
2023-03-10 10:55:26 +0300

    fix size of logo

--- src/components/SolanaIcon.js
+++ src/components/SolanaIcon.js
@@ -3,6 +3,6 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 
 export default function SolanaIcon() {
   return (
-    <img src='https://i.imgur.com/oqp8Cbo.png' style={{ width: 50, height: 50 }} ></img>
+    <img src='https://i.imgur.com/oqp8Cbo.png' style={{ width: 20, height: 20 }} ></img>
   );
 }
